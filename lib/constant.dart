c73b02b1a5cb8895c98078a7fcfb76d4ee9c24a9
Salman <salman.naeem.112215@gmail.com>
2025-04-19 20:50:41 +0500

    Completed App

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,1 +1,5 @@
-const String kServerUrl = 'http://192.168.10.5:8083/predict/';
+// String url = '192.168.10.5:8083';
+String url = '135.235.216.175:8080';
+String kServerUrl = 'http://$url/predict/';
+String kPingUrl = 'http://$url/ping';
+// const String kServerUrl = 'http://135.235.216.175:8080/predict/';
--- lib/screens/image_selection_page.dart
+++ lib/screens/image_selection_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
@@ -69,53 +70,77 @@ class _ImageSelectionScreenState extends State<ImageSelectionScreen> {
     }
   }
 
+  bool isUplaoding = false;
   Future<void> uploadImage(BuildContext context, File imageFile) async {
     final uri = Uri.parse(kServerUrl);
-    final request = http.MultipartRequest('POST', uri);
+    final pingUri = Uri.parse(kPingUrl);
+
+    setState(() {
+      isUplaoding = true;
+    });
+
+    await Future.delayed(Duration(seconds: 1));
 
     try {
+      // 👇 Check if server is alive first (fast ping)
+      final pingResponse =
+          await http.get(pingUri).timeout(Duration(seconds: 3));
+      if (pingResponse.statusCode != 200) {
+        throw Exception("Server is unreachable");
+      }
+      print("SERVER REACHABLE");
+
+      final request = http.MultipartRequest('POST', uri);
       request.files.add(
         await http.MultipartFile.fromPath(
-          'file', // key name in form-data
+          'file',
           imageFile.path,
           filename: basename(imageFile.path),
         ),
       );
 
-      final response = await request.send();
+      final streamedResponse =
+          await request.send().timeout(Duration(seconds: 30));
 
-      if (response.statusCode == 200) {
-        final responseBody = await response.stream.bytesToString();
+      if (streamedResponse.statusCode == 200) {
+        final responseBody = await streamedResponse.stream.bytesToString();
         final json = jsonDecode(responseBody);
-        debugPrint("RESPONSE $json");
         final result = json['result'];
+
+        setState(() {
+          isUplaoding = false;
+        });
+
         if (result == 'Abnormal Feet') {
           Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ResultDiagnosisOfAbnormalFoot(),
-            ),
-          );
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ResultDiagnosisOfAbnormalFoot()));
         } else if (result == 'Normal Feet') {
           Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ResultDiagnosisOfNormalFoot(
-                diabetesDuration: widget.diabetesDuration,
-                footCare: widget.footCare,
-                hadUlcerBefore: widget.hadUlcerBefore,
-                imagePath: _selectedImage!.path,
-              ),
-            ),
-          );
+              context,
+              MaterialPageRoute(
+                builder: (context) => ResultDiagnosisOfNormalFoot(
+                  diabetesDuration: widget.diabetesDuration,
+                  footCare: widget.footCare,
+                  hadUlcerBefore: widget.hadUlcerBefore,
+                  imagePath: _selectedImage!.path,
+                ),
+              ));
         } else {
           showError(context, 'Please Select Foot');
         }
       } else {
+        setState(() {
+          isUplaoding = false;
+        });
         showError(context, 'Image Uploading Failed');
       }
     } catch (e) {
-      showError(context, 'Image Uploading Failed');
+      setState(() {
+        isUplaoding = false;
+      });
+      showError(context, 'Server not responding or down.');
     }
   }
 
@@ -128,50 +153,54 @@ class _ImageSelectionScreenState extends State<ImageSelectionScreen> {
             style: TextStyle(color: Colors.white)),
         backgroundColor: const Color(0xFF33657D),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            if (_selectedImage != null)
-              Image.file(
-                _selectedImage!,
-                height: 224,
-                width: 224,
-              )
-            else
-              Container(
-                height: 250,
-                color: Colors.grey[300],
-                child: const Center(child: Text("No Image Selected")),
+      body: isUplaoding
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  if (_selectedImage != null)
+                    Image.file(
+                      _selectedImage!,
+                      height: 224,
+                      width: 224,
+                    )
+                  else
+                    Container(
+                      height: 250,
+                      color: Colors.grey[300],
+                      child: const Center(child: Text("No Image Selected")),
+                    ),
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: () => _pickImage(context, ImageSource.gallery),
+                    icon: const Icon(Icons.photo_library),
+                    label: const Text("Pick from Gallery"),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton.icon(
+                    onPressed: () => _pickImage(context, ImageSource.camera),
+                    icon: const Icon(Icons.camera_alt),
+                    label: const Text("Take a Photo"),
+                  ),
+                  const Spacer(),
+                  ElevatedButton(
+                    onPressed: _selectedImage == null
+                        ? null
+                        : () => uploadImage(context, _selectedImage!),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF33657D),
+                      foregroundColor: Colors.white,
+                      minimumSize: const Size.fromHeight(50),
+                      textStyle: const TextStyle(fontSize: 18),
+                    ),
+                    child: const Text("Submit"),
+                  ),
+                ],
               ),
-            const SizedBox(height: 20),
-            ElevatedButton.icon(
-              onPressed: () => _pickImage(context, ImageSource.gallery),
-              icon: const Icon(Icons.photo_library),
-              label: const Text("Pick from Gallery"),
             ),
-            const SizedBox(height: 10),
-            ElevatedButton.icon(
-              onPressed: () => _pickImage(context, ImageSource.camera),
-              icon: const Icon(Icons.camera_alt),
-              label: const Text("Take a Photo"),
-            ),
-            const Spacer(),
-            ElevatedButton(
-              onPressed: _selectedImage == null
-                  ? null
-                  : () => uploadImage(context, _selectedImage!),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF33657D),
-                foregroundColor: Colors.white,
-                minimumSize: const Size.fromHeight(50),
-                textStyle: const TextStyle(fontSize: 18),
-              ),
-              child: const Text("Submit"),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
